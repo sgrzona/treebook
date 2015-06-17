196e986ad1d155a70efb907f837088889194ab15
Stephanie Grzona <stephanie@800ceoread.com>
2015-06-17 14:37:47 -0500

    Styled Falsh Messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
   resources :statuses
   root to: 'statuses#index'
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	    test "a user should enter a first name" do 
+		  user = User.new
+		  assert !user.save
+		  assert !user.errors[:first_name].empty?
+	    end
+
+	 	test "a user should enter a last name" do 
+		  user = User.new
+		  assert !user.save
+		  assert !user.errors[:last_name].empty?
+	    end
+
+	  	test "a user should enter a profile name" do 
+		  user = User.new
+		  assert !user.save
+		  assert !user.errors[:profile_name].empty?
+	    end
+
+	 	test "a user should have a unique profile name" do
+	 	  user = User.new
+	 	  user.profile_name = "scgrzona"
+	      user.email = "stephanie@800ceoread.com"
+	 	  user.first_name = "Stephanie"
+	      user.last_name = "Grzona"
+	 	  	 	  
+	 	  assert !user.save
+	 	  assert !user.errors[:profile_name].empty? 
+	    end
 end
+
+
+
